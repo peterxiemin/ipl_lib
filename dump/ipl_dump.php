ae459776c18a21291801e87f3236d6899e29c64c
Damian <peterxiemin@163.com>
2015-01-15 16:15:32 +0800

    new file:   dump/ipl_dump.php

---
+++ dump/ipl_dump.php
@@ -0,0 +1,17 @@
+<?
+/*
+ *
+ *  date: 2015-01-15
+ *  author : xiemin
+ *  descript : 格式化输出数据结构 
+ *
+ *
+ *
+ */
+
+function ipl_dump($msg)
+{
+	echo '<pre>';
+	var_dump($msg);
+	echo '</pre>';
+}
